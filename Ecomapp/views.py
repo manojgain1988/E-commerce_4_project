e71c38fab61180e578439962d978fbd918cf918c
manojgain1988 <manojgain1988@gmail.com>
2024-11-23 22:12:12 +0600

    first commit

--- Ecomapp/views.py
+++ Ecomapp/views.py
@@ -7,8 +7,13 @@ from Products.models import Product
 def Index(request):
     setting = Setting.objects.get(id=1)
     products = Product.objects.all().order_by('id')[:4]
+    latest_products = Product.objects.all().order_by('-id')
+    featured_products = Product.objects.all()
+    
     context={
         'setting':setting,
         'products':products,
+        'latest_products':latest_products,
+        'featured_products':featured_products,
     }
     return render(request,'home.html',context)  
